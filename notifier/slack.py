d62a68c6e23406643d961a3b8df21436f041a57c
Brett Graham <brettgraham@gmail.com>
2017-07-12 14:52:31 -0400

    bringing out token

--- notifier/slack.py
+++ notifier/slack.py
@@ -5,9 +5,11 @@ import os
 import slackclient
 
 
-def channel_message(text, channel):
+def channel_message(text, channel, token=None):
     """Requires a slack token stored in the SLACK_TOKEN environment variable"""
-    c = slackclient.SlackClient(os.environ['SLACK_TOKEN'])
+    if token is None:
+        token = os.environ['SLACK_TOKEN']
+    c = slackclient.SlackClient(token)
     c.api_call(
         'chat.postMessage',
         channel=channel,
